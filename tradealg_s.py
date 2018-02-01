9a15f52e92714a18b6bc87b9800b98173666ec97
Gauleria <gauleria@gmail.com>
2018-02-01 12:57:30 +0100

    added tradealg_s.py - beta

--- tradealg_s.py
+++ tradealg_s.py
@@ -1,19 +1,31 @@
 #!/bin/python3
 import sqlite3
 import time
+import json
+DBconf = 'db.conf'
+DBname = open(DBconf).readline().strip()
+
+
 HISTORY = 1800#30 min epoch
+wall = 'wallet'
 
-conn = sqlite3.connect('trade.db')
-c = conn.cursor()
-delta = time.time()-HISTORY
-string = "select sell_price from TICKER where pair='BTC_USD' AND updated>%s;"%delta
-c.execute(string)
-result = c.fetchall()
-for line in result: print (line)
-deff = float(result[0][0])-float(result[-1][0]) 
-if deff >= 0:print ("GROWS UP TO %s USD"%deff)
-else: print ("FALL DOWN TO %s USD"%deff)
-#print (int(result))
-conn.commit()
-conn.close()
-
+def getCurrency(pair, act, period=0):
+	if period == 0:string = "select  %s_price,updated from TICKER where pair='%s' limit 1"%(act,pair)
+	else: string = "select sell_price,updated from TICKER where pair='BTC_USD' AND updated>%s;"%(time.time()-period*60)
+	conn = sqlite3.connect(DBname)
+	c = conn.cursor()
+	c.execute(string)
+	res = c.fetchall()
+	conn.commit()
+	conn.close()
+	return res
+def wallet(pair, act, wall):
+	fwall = open(wall)
+	json.loads(fwall)
+	fwall.close()
+result = getCurrency("BTC_USD","buy",30)
+first = float(result[0][0])
+last = float(result[-1][0])
+deff = last-first
+if deff >= 0:print ("%s -> %s GROWS UP TO %s USD or %s perc."%(first,last,deff,(deff/last*100)))
+else: print ("%s -> %s FALL DOWN TO %s USD or %s perc."%(first,last,deff,(deff/last*-100)))
